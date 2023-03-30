c9d01faed6687242b19bf77b78aaa98bee29f515
ajhwan <129160008+ajhwan@users.noreply.github.com>
2023-03-30 11:37:36 +0900

    밝기가 증가하는 코드

--- digital_light.ino
+++ digital_light.ino
@@ -8,10 +8,15 @@ void setup() {
 
 void loop() {
   for (int t_high = 0; t_high<=10;t_high++){
-    digitalWrite(LED, HIGH);
-    delay(t_high);
-    digitalWrite(LED,LOW);
-    delay(10-t_high);
+    int cnt = 0;
+    while(true) {
+      digitalWrite(LED, HIGH);
+      delay(t_high);
+      digitalWrite(LED, LOW);
+      delay(10-t_high);
+      cnt++;
+      if (cnt == 10) break;
+    }
   }
   //digitalWrite(LED, HIGH);
   //delay(9);
