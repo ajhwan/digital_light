2f7f09a5bd03d89f85ea952ccd809770649216ca
ajhwan <129160008+ajhwan@users.noreply.github.com>
2023-03-30 10:40:39 +0900

    Update digital_light.ino
    blink_LED

--- digital_light.ino
+++ digital_light.ino
@@ -3,10 +3,12 @@ const int LED = 13;
 void setup() {
   pinMode(LED, OUTPUT);
   //digitalWrite(LED, HIGH);
-  digitalWrite(LED, LOW);
+  //digitalWrite(LED, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
 }
